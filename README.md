a78fda2892654b5766b4f7ef22185eb7094cb43e
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-06-06 10:02:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project E-commerce
+
+<p align="center">
+  <img src="src/assets/imgReadme.png" width="600" title="github-user">
+  <alt="accessibility text">
+</p>
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+import Auth from "./AuthWrapper";
+
+export {
+  About,
+  Auth,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+};
